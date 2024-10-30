b053e34d76299c2c85091b4d21ac3d52c0cbdc1d
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-10-29 21:58:24 -0700

    similar components desin and responsice done

--- src/Components/Pages/Details/Details.jsx
+++ src/Components/Pages/Details/Details.jsx
@@ -3,24 +3,43 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { TbCoinTakaFilled, TbBrandAirtable } from "react-icons/tb";
+import { SiSimilarweb } from "react-icons/si";
+import ProductCart from '../../SharedPages/ProductCart/ProductCart';
 
 const Details = () => {
     const {id} =useParams()
     const [product,setProduct]=useState({})
+    const [similarProduct,setSimilarProduct]=useState([])
+
    useEffect(()=>{
     axios.get(`http://localhost:5000/products/singleData/?id=${id}`)
     .then(res => setProduct(res.data))
     
    },[id])
-   console.log("single Product is console",product);
+   useEffect(()=>{
+    const fetchData =async()=>{
+      try {
+       const res= await axios.get(`http://localhost:5000/products/similarData/?category=${product.category}&brand=${product.brand}`)
+       setSimilarProduct(res.data)
+        
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    fetchData()
+   
+    
+   },[product.category, product.brand])
+   console.log("similar Product is console",similarProduct);
     
     return (
-        <div className=' h-screen'>
-            <div className='bg-white flex justify-around my-10 rounded-xl px-5 py-10'>
-            <div className='w-[30%]'>
+        <div className=''>
+            <div className='bg-white md:flex justify-around my-10 rounded-xl px-5 py-10'>
+            <div className='md:w-[30%]'>
                 <img src={product.img} alt={product.title} className='w-[300px] h-[300px]'/>
             </div>
-            <div  className='w-[30%]'>
+            <div  className='md:w-[30%]'>
             <h2 className='text-2xl font-bold text-black'>{product.title}</h2>
             <p className='flex items-center gap-x-3 text-blue-400'> <TbBrandAirtable /> {product.brand} </p>
             <h3 className='text-xl font-semibold text-black mt-3'>Description</h3>
@@ -31,7 +50,7 @@ const Details = () => {
             </div>
 
             </div>
-            <div  className='w-[30%]'>
+            <div  className='md:w-[30%]'>
             <h2 className='text-2xl font-bold text-black'>Specifications</h2>
             <div className="overflow-x-auto">
   <table className="table border mt-2">
@@ -66,6 +85,17 @@ const Details = () => {
             </div>
 
             
+            </div>
+
+            <div>
+              <h2 className='text-2xl font-bold text-black flex items-center gap-x-3 ml-7 md:ml-0'><SiSimilarweb className='text-blue-400' /> Similar Products</h2>
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mt-3 mb-5 mx-5 md:mx-0'>
+                {
+                  similarProduct.map(simiPro => <ProductCart key={simiPro._id} product={simiPro} />)
+                }
+
+              </div>
+
             </div>
             
             
